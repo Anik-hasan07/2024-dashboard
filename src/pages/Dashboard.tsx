bf2a6d6dfd087111959040c2712f7474d6cd3f51
Anik-hasan07 <iamakashcse@gmail.com>
2024-01-28 19:06:21 +0600

    feature/main

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import AdminSidebar from '../components/AdminSidebar'
+
+const Dashboard = () => {
+  return (
+    <div className="admin-container">
+
+      <AdminSidebar/>
+      <main className="dashboard">
+        
+      </main>
+      </div>
+  )
+}
+
+export default Dashboard
